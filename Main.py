fa11fb4bdc88e42c12c3faa3183be0cc98665044
royeretta <r.yangali@pucp.pe>
2021-09-10 01:58:45 -0500

    aumentando features a mostrar

--- Main.py
+++ Main.py
@@ -18,16 +18,33 @@ conn = openstack.connect(cloud="openstack")
 f = open("OutputRoyer.txt","w")
 ###
 # Listamos las instancias que están creadas:
+f.write("Lista de instancias creadas:")
 for server in conn.compute.servers():
     print(server.name)
     f.write(server.name + " | ")
 
 f.write("\n")
+
+f.write("Lista de imágenes disponibles:")
 # Listamos las imágenes disponibles:
 for image in conn.compute.images():
     print(image.name)
     f.write(image.name + " | ")
-
 f.write("\n")
+
+f.write("Lista de flavors disponibles:")
+# Listamos los flavors disponibles:
+for flavor in conn.compute.flavors():
+    print(flavor.name)
+    f.write(flavor.name + " | ")
+f.write("\n")
+
+f.write("Lista de endpoints:")
+# Listamos los endpoints:
+for endpoints in conn.identity.endpoints():
+    print(endpoints)
+    f.write(endpoints + " | ")
+f.write("\n")
+
 
 f.close()
