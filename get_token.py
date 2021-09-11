81cc454e37f84b55acbf62ded0574e73e446c2a9
royeretta <r.yangali@pucp.pe>
2021-09-10 21:39:00 -0500

    tengo internettt????

--- get_token.py
+++ get_token.py
@@ -31,8 +31,11 @@ payload = { ###estos valores están detallados en el archivo admin-openrc.sh que
 res = requests.post('http://192.168.0.80:5000/v3/auth/tokens',
                         headers = {'content-type':'application/json'},
                         data=json.dumps(payload))
-print(res.headers) ###Parámetro X-Subject-Token es el que tiene el token, lo ideal sería automatizarlo para que se obtenga solo este token y pase a "Main.py"
-##con ese valor de token ya se podrían realizar solicitudes a los distintos apis de servicios de openstack como glance, entre otros.
+print(res.headers) ###Parámetro X-Subject-Token es el que tiene el token, lo ideal sería automatizarlo para que se obtenga este token y pase automáticamente a "Main.py"
+##con ese valor de token ya se podrían realizar solicitudes a los distintos apis de servicios de openstack como glance, entre otros de manera automática.
 
+#Por cuestiones de tiempo, se procede con el método detallado a continuación:
+###Una alternativa es la de ejecutar este script "get_token.py" para luego obtener el token del output y manualmente copiarlo y pegarlo en las solicitudes GET y POST
+##que se realizan en el script "Main.py"
     
     
